4dc56c26f59d37d04a812a79db5d254eeaece1b3
Jovany <140675889+VeryRealDinosaur@users.noreply.github.com>
2025-03-31 22:15:38 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/bomF/', // Updated to match your repository name
 })
